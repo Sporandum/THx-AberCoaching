8dcfa914a409b355d215f1a874e1ba7a7f46fb75
Sporandum <sporandum@gmail.com>
2020-03-02 12:37:36 +0100

    Modify output directory dist > assets

---
+++ assets/css/styles-9ec6554b.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * THx-AberCoaching
+ * 
+ * @author Sporandum
+ * @version 0.1.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2020 Sporandum
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackiowpackcssJsonp=window.wpackiowpackcssJsonp||[]).push([[1],[function(c,n,o){o(1),c.exports=o(2)},function(c,n,o){"use strict";var s="wpackassets".replace(/[^a-zA-Z0-9_-]/g,"");o.p=window["__wpackIo".concat(s)]},function(c,n,o){"use strict";o.r(n);o(3),o(4)},function(c,n,o){},function(c,n,o){}],[[0,0]]]);
+//# sourceMappingURL=styles-9ec6554b.js.map
