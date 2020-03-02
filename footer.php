df6ae9aedbb18eaa3ce95e2cd2112f0d9c02207b
Sporandum <sporandum@gmail.com>
2020-03-02 15:40:37 +0100

    Modify helpers css

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <div class="site-footer">
 	<div class="site-footer__row site-footer__row--bg-color">
-		<div class="container container--y-center">
+		<div class="container d-flex d-flex--align-center">
 			<div>RDV au cabinet : 8 rue Villebois-Mareuil - LE RELECQ KERHUON</div>
 			<div class="ml-auto">06 32 75 57 90</div>
 		</div>
 	</div>
 	<div class="site-footer__row site-footer__row--higher">
-		<div class="container container--y-center">
+		<div class="container d-flex d-flex--align-center">
 			<div class="site-footer__brand-logo"><img src="<?php echo get_theme_file_uri('/assets/images/logo_abercoaching_inline.png'); ?>" alt=""></div>
 			<div class="site-footer__social-icons">
 				<div class="site-footer__social-icon"><i class="fab fa-facebook-square fa-3x"></i></div>
@@ -15,7 +15,7 @@
 		</div>
 	</div>
 	<div class="site-footer__row site-footer__row--border-top">
-		<div class="container container--y-center">
+		<div class="container d-flex d-flex--align-center">
 			<ul class="site-footer__bottom-link">
 				<li><a href="#">Mentions Légales</a></li>
 				<li><a href="#">contact@abercoaching.com</a></li>
