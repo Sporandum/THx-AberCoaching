207cc32e78b06e643558a3850ef730e3e685388c
Sporandum <sporandum@gmail.com>
2020-08-14 10:00:34 +0200

    Archive builded

--- package/wpack/inc/abercoaching-post-types.php
+++ package/wpack/inc/abercoaching-post-types.php
@@ -1,33 +1,36 @@
 <?php
 // Custom post types
-function abercoaching_post_types() {
-	// Services
-	register_post_type('services', array(
-		'public' => true,
-		'menu_icon' => 'dashicons-format-aside',
-		'show_in_rest' => true, // For use block editor
-		'supports' => array('title', 'editor', 'thumbnail'),
-		'labels' => array(
-			'name' => 'Services',
-			'add_new_item' => 'Ajouter un service',
-			'edit_item' => 'Editer un service',
-			'all_items' => 'Services',
-			'singular_name' => 'Service'
-		)
-	));
+if (!function_exists('abercoaching_post_types')) {
 
-	register_post_type('testimonials', array(
-		'public' => true,
-		'menu_icon' => 'dashicons-format-quote',
-		'supports' => array('title'),
-		'labels' => array(
-			'name' => 'Témoignages',
-			'add_new_item' => 'Ajouter un témoignage',
-			'edit_item' => 'Editer un témoignage',
-			'all_items' => 'Témoignages',
-			'singular_name' => 'Témoignage'
-		)
-	));
+	function abercoaching_post_types() {
+		// Services
+		register_post_type('services', array(
+			'public' => true,
+			'menu_icon' => 'dashicons-format-aside',
+			'show_in_rest' => true, // For use block editor
+			'supports' => array('title', 'editor', 'thumbnail'),
+			'labels' => array(
+				'name' => 'Services',
+				'add_new_item' => 'Ajouter un service',
+				'edit_item' => 'Editer un service',
+				'all_items' => 'Services',
+				'singular_name' => 'Service'
+			)
+		));
+	
+		register_post_type('testimonials', array(
+			'public' => true,
+			'menu_icon' => 'dashicons-format-quote',
+			'supports' => array('title'),
+			'labels' => array(
+				'name' => 'Témoignages',
+				'add_new_item' => 'Ajouter un témoignage',
+				'edit_item' => 'Editer un témoignage',
+				'all_items' => 'Témoignages',
+				'singular_name' => 'Témoignage'
+			)
+		));
+	}
+	
+	add_action('init', 'abercoaching_post_types');
 }
-
-add_action('init', 'abercoaching_post_types');
